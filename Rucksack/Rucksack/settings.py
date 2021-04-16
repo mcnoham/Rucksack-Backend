e4b183b20d2bc94f914727c4e687f1afe31dc89e
mcnoham <mikemcwfb@gmail.com>
2021-04-16 15:26:17 -0500

    Update settings.py
    fixed cors header middleware

--- Rucksack/Rucksack/settings.py
+++ Rucksack/Rucksack/settings.py
@@ -75,6 +75,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
