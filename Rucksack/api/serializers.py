95374c19eb3e9e0a18c6dcae18b30dde0a479416
Blindau2 <blindau2@uwm.edu>
2021-04-21 10:12:49 -0500

    added rating and number of ratings to Itinerary, added Rating model to the database, updated serializers accordingly

--- Rucksack/api/serializers.py
+++ Rucksack/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, Profile, Itinerary
+from .models import User, Profile, Itinerary, Rating
 from django import forms
 
 
@@ -24,4 +24,9 @@ class ItinerarySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Itinerary
-        fields = ('user', 'title', 'budget', 'duration_magnitude', 'description', 'transportation_tag', 'accommodation_tag', 'location_tag')
+        fields = ('user', 'title', 'budget', 'duration_magnitude', 'description', 'transportation_tag', 'accommodation_tag', 'location_tag', 'rating', 'number_of_ratings')
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ('user', 'itinerary', 'rating')
